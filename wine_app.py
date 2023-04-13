dbef9625879781dc4cdbee0050588ed7c436ecc8
Shruti <112333555+ShruK1703@users.noreply.github.com>
2023-04-14 00:58:19 +0530

    Update wine_app.py

--- wine_app.py
+++ wine_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 #loading the saved model
-loaded_model = pickle.load(open(r'C:\Users\USER\Downloads\New folder\trained_model.sav','rb'))
+loaded_model = pickle.load(open(r'C:\Users\USER\trained_model.sav','rb'))
 
 #creating a function for prediction
 def wine_quality_prediction(data):
