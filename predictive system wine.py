d9f761adda47fd217fa2f88a76bf689ec6dfbae9
Shruti <112333555+ShruK1703@users.noreply.github.com>
2023-04-14 10:05:16 +0530

    Update predictive system wine.py

--- predictive system wine.py
+++ predictive system wine.py
@@ -8,7 +8,7 @@ import numpy as np
 import pickle
 
 #loading the saved model
-loaded_model = pickle.load(open(r'C:\Users\USER\Downloads\New folder\trained_model.sav','rb'))
+loaded_model = pickle.load(open(r'C:\Users\USER\trained_model.sav','rb'))
 
 
 data = (4.2,0.215,0.23,1.808289,0.041,64.0,157.0,0.99688,3.42,0.44,8.0)
