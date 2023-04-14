d0e5d2fe77ddff4a54f45600629ce54682096339
Shruti <112333555+ShruK1703@users.noreply.github.com>
2023-04-14 10:00:13 +0530

    Update wine_app.py

--- wine_app.py
+++ wine_app.py
@@ -61,17 +61,7 @@ def main():
         
     
     st.success(Quality)
-    
-    
-    
-    uploaded_file = st.file_uploader(
-    "Choose your database", accept_multiple_files=False)
-if uploaded_file is not None:
-    file_name = uploaded_file
-else:
-    file_name = "C:\Users\USER\Downloads\New folder\trained_model.sav"
-    
-    
+     
     
     
     
